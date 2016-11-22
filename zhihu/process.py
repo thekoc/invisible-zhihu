3aa814a80138ab86415322f92741ee23dcd5056c
koc <thekoc.z@hotmail.com>
2016-11-22 13:25:19 +0800

    拆分模块

--- zhihu/process.py
+++ zhihu/process.py
@@ -1,39 +1,10 @@
-import requests
-import os
 import shutil
-import json
 import time
-from bs4 import BeautifulSoup
 from tools import qid_to_url
 from tools import aid_to_url
 from tools import tid_to_url
 from tools import uid_to_url
-from zhihu import ZhihuClient as WebClient
 from archive import ZhihuDatabase
-
-
-class QuestionSpider(object):
-    def __init__(self):
-        self.web_client = WebClient()
-        self.cookies_path = 'cookies.json'
-        self.cookies = json.load(open(self.cookies_path))
-        if os.path.isfile(self.cookies_path):
-            self.web_client.login_with_cookies(self.cookies_path)
-        else:
-            self.web_client.create_cookies(self.cookies_path)
-
-    def get_new_question_urls(self):
-        question_urls = []
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-        }
-        s = requests.Session()
-        req = s.get('https://www.zhihu.com/topic/19673476/newest', headers=headers, cookies=self.cookies)
-        soup = BeautifulSoup(req.text, 'html.parser')
-        host = 'https://www.zhihu.com'
-        for t in soup.find_all('a', class_='question_link'):
-            question_urls.append(host + t['href'])
-        return question_urls
 
 
 class QuestionProcessor(object):
